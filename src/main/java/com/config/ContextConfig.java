097fc74da112f25c5d0bb4d73817806bf6c494bc
ArkJuanPa <martinezroserojuanpablo@gmail.com>
2026-08-30 20:59:26 -0500

    Exercise with config

---
+++ src/main/java/com/config/ContextConfig.java
@@ -0,0 +1,38 @@
+package com.config;
+
+import com.repository.ArtistRepositoryImpl;
+import com.repository.IArtistRepository;
+import com.repository.ITrackRepository;
+import com.repository.TrackRepositoryImpl;
+import com.service.ArtistService;
+import com.service.IArtistService;
+import com.service.ITrackService;
+import com.service.TrackService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ContextConfig {
+
+    // Repositories
+    @Bean(initMethod = "initialize")
+    public TrackRepositoryImpl trackRepository() {
+        return new TrackRepositoryImpl();
+    }
+
+    @Bean(initMethod = "initialize")
+    public ArtistRepositoryImpl artistRepository() {
+        return new ArtistRepositoryImpl();
+    }
+
+    // Services
+    @Bean
+    public ArtistService artistService(IArtistRepository artistRepository, ITrackRepository trackRepository) {
+        return new ArtistService(artistRepository, trackRepository);
+    }
+
+    @Bean
+    public ITrackService trackService(ITrackRepository trackRepository, IArtistRepository artistRepository) {
+        return new TrackService(trackRepository, artistRepository);
+    }
+}
